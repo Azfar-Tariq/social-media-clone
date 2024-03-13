2be62e16c653bcc46a6798f5fb13f9843f181a8b
Azfar-Tariq <azfaro2031@gmail.com>
2024-03-13 23:14:17 +0500

    Passing token data

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -3,7 +3,7 @@ export default function UserProfile({ params }: any) {
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1 className="text-center text-white text-2xl">Profile</h1>
       <hr />
-      <p className="text-4xl">Profile page</p>
+      <p className="text-4xl flex">Profile page</p>
       <span className="p-2 rounded-lg bg-emerald-400 text-black">
         {params.id}
       </span>
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,35 @@
+"use client";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+
 export default function ProfilePage() {
+  const router = useRouter();
+
+  const logout = async () => {
+    try {
+      const response = await axios.get("/api/users/logout");
+      console.log(response.data);
+      toast.success("Logout successful!");
+      router.push("/login");
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1 className="text-center text-white text-2xl">Profile</h1>
       <hr />
       <p>Profile page</p>
+      <hr />
+      <button
+        className="p-2 m-2 rounded-lg bg-emerald-500 hover:bg-emerald-600"
+        onClick={logout}
+      >
+        Logout
+      </button>
     </div>
   );
 }
