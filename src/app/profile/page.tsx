348c71ce340f16d0376758b8af499a1a75f54260
Azfar-Tariq <azfaro2031@gmail.com>
2024-05-03 18:52:17 +0500

    Middleware added

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,23 @@
 "use client";
 import axios from "axios";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import toast from "react-hot-toast";
 
 export default function ProfilePage() {
   const router = useRouter();
+  const [user, setUser] = useState("");
+
+  async function getData() {
+    const response = await axios.get("/api/users/me");
+    console.log(response.data);
+    setUser(response.data.data._id);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const logout = async () => {
     try {
@@ -23,6 +36,17 @@ export default function ProfilePage() {
       <h1 className="text-center text-white text-2xl">Profile</h1>
       <hr />
       <p>Profile page</p>
+      <h2>
+        {user === "" ? (
+          "User Not Found"
+        ) : (
+          <Link href={`/profile/${user}`}>
+            <div className="px-2 py-1 bg-amber-300 text-black rounded-lg hover:underline">
+              ID: {user}
+            </div>
+          </Link>
+        )}
+      </h2>
       <hr />
       <button
         className="p-2 m-2 rounded-lg bg-emerald-500 hover:bg-emerald-600"
